02717627925b2b89ae52860b38f1eb2b3e660298
NathanSantacana <santacana.nathan@gmail.com>
2014-11-27 16:24:46 +0100

    re-modif

--- src/Controleur/ControleurPrincipal.java
+++ src/Controleur/ControleurPrincipal.java
@@ -1,12 +1,46 @@
 package Controleur;
 
+import Interface.FenetrePrincipal;
 import Model.Importer;
 
+/**
+ * Permet le lancement de l'application ainsi que d'effectuer les différents traitements
+ * @author Benjamin
+ *
+ */
 public class ControleurPrincipal {
-
-	public static String getTexte() {
-		// TODO Auto-generated method stub
-		return Importer.importerTexte("textes/texteTest.txt");
+	private String texteNonModifie;
+	private String texteAffiche;
+	private String RegExp;
+	private String texteATrouver;
+	private String texteModifie;
+	/**
+	 * Lancement de l'application
+	 */
+	public ControleurPrincipal() {
+		// TODO Auto-generated constructor stub
+		new FenetrePrincipal(this);
 	}
+	/**
+	 * Permet d'obtenir le texte issu d'un fichier texte transposé au format HTML
+	 * @return Une String comportant le texte issu d'un fichier texte transposé au format HTML
+	 */
+	public String getTexteAfficher() {
+		// TODO Auto-generated method stub
+		texteNonModifie=Importer.importerTexte("textes/texteTest.txt");
+		//RegExp=Importer.importerTexte("textes/texteTest.regexp");
+		//texteModifie=AjoutCouleur.ajouterCouleur(texteNonModifie, RegExp);
+		//System.out.println("cc");
+		//System.out.println(texteModifie);
+		texteAffiche=("<html>"+texteNonModifie+"</html>").replaceAll("\n", "<br/>");
+		//System.out.println(texteAffiche);
+		
+		return texteAffiche;
+	}
+	
+	public static void main(String[] args){
+		new ControleurPrincipal();
+	}
+	
 
 }
--- src/Interface/FenetrePrincipal.java
+++ src/Interface/FenetrePrincipal.java
@@ -5,13 +5,16 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.swing.*;
 
+import Controleur.AjoutCouleur;
 import Controleur.ControleurPrincipal;
-
+/**
+ * Permet l'affichage de l'interface utilisateur
+ * @author Benjamin
+ *
+ */
 public class FenetrePrincipal extends JFrame {
 	private JMenuBar barreDeMenu=new JMenuBar();
 	private JMenu aide=new JMenu("Aide");
@@ -19,16 +22,16 @@ public class FenetrePrincipal extends JFrame {
 	private JFrame cetteFenetre;
 	private JLabel texte;
 	private JTextField champDeSaisie;
-	private String aTrouver;
-	private JButton BoutonDeValidation=new JButton("Valider");
-	private JButton BoutonDeReponse=new JButton("Reponse");
+	private ControleurPrincipal monControleur;
 	/**
-	 * @param args
+	 * Affiche la fenetre principale de l'application
+	 * @param controleurPrincipal Le controleurPrincipal de l'application
+	 * 
 	 */
-	public FenetrePrincipal(){
+	public FenetrePrincipal(ControleurPrincipal controleurPrincipal){
 		super("RegExp");
 		cetteFenetre=this;
-		this.setIconImage(new ImageIcon("logo.png").getImage());
+		monControleur=controleurPrincipal;
 		this.setSize(new Dimension(500,400));
 		this.setLayout(new BorderLayout());
 		
@@ -38,6 +41,7 @@ public class FenetrePrincipal extends JFrame {
 		aide.add(boutonAPropos);
 		
 		boutonAPropos.addActionListener(new ActionListener() {
+			
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				A_propos fenetreApropos = new A_propos(cetteFenetre, "A Propos");
@@ -45,94 +49,39 @@ public class FenetrePrincipal extends JFrame {
 			}
 		});
 		
-		texte=new JLabel(ControleurPrincipal.getTexte());
-	
-		// ON FOUT LE TEXTE A TROUVER DANS LE STRING
-				// ON CHERCHE HTML
-				
-				aTrouver=ControleurPrincipal.getTexte();
-				String caca = "";
-				String caca2 = "";
-				String caca3 = "";
-				//String texteATester ="<html>Ceci est <font color = red>Squalala</font> le déstructeur ténébreux des 7 chevaliers  de l'apocalypse</html>";
-			
-				String texteATrouver ="est <font color = red>[^.]+<.font> le";
-				Pattern p = Pattern.compile(texteATrouver);
-				Matcher m = p.matcher(aTrouver);
-				StringBuffer sb = new StringBuffer();
-				while (m.find()) {
-					 System.out.println("gg");
-				     m.appendReplacement(sb, "lol"+m.group()+"lol");
-				     caca=m.group();
-				     caca2=caca.replaceAll("<font color = red>", ""); 
-				     caca3=caca2.replaceAll("</font>", ""); 
-				     
-				 }
-				 String texteATrouverNathan =texteATrouver.replaceAll("<font color = red>", "");
-				 String texteATrouverNathanFinal =texteATrouverNathan.replaceAll("<.font>", ""); 
-				 m.appendTail(sb);
-				/* System.out.println("bwaa");
-				 System.out.println(texteATrouverNathanFinal);
-				 System.out.println("bwaa");
-				 System.out.println(sb.toString());
-				 System.out.println(caca);
-				 System.out.println(caca3);*/
-				
-				 // FAUT TROUVER NATHAN FINAL COUNTDOWN
-				 //est [^.]+ le
-		
-
-		////////////////////////////////////////
-		
+		texte=new JLabel(monControleur.getTexteAfficher());
 		JPanel panMilieu=new JPanel();
 		this.add(panMilieu,BorderLayout.CENTER);
 		panMilieu.setLayout(new FlowLayout());
 		panMilieu.add(texte);
 		
+		
+		
 		JPanel panSud=new JPanel();
 		champDeSaisie=new JTextField();
 		champDeSaisie.setPreferredSize(new Dimension(200,30));
-		/*
-		panSud.setLayout(new GridLayout(2,1));
-		
 		panSud.add(champDeSaisie);
-		
-		champDeSaisie.setPreferredSize(new Dimension(200,30));
-		JPanel panBouton=new JPanel();
-		panBouton.setLayout(new FlowLayout());
-		panBouton.add(BoutonDeValidation);
-		panBouton.add(BoutonDeReponse);
-		panSud.add(panBouton);
-		*/
-		panSud.add(champDeSaisie);
+		JButton BoutonDeValidation=new JButton("Valider");
 		panSud.add(BoutonDeValidation);
-		panSud.add(BoutonDeReponse);
+		
+		//ON ajoute la couleur de BASE 
+		//texte.setText(AjoutCouleur.ajouterCouleur(monControleur.getTexteAfficher(), champDeSaisie.getText()));
+		
 		
 		BoutonDeValidation.addActionListener(new ActionListener() {
+			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				//texte.setText(AjoutCouleur.ajouterCouleur(texte.getText(), champDeSaisie.getText()));
-				if(champDeSaisie.getText().equals("est [^.]+ le")){
-					
-					JOptionPane.showMessageDialog(null, "Bonne réponse !");
-				}else{
-					JOptionPane.showMessageDialog(null, "Mauvaise réponse !");
-				}
-			}
-		});
-		BoutonDeReponse.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "La réponse est : est [^.]+ le");
+			texte.setText(AjoutCouleur.ajouterCouleur(monControleur.getTexteAfficher(), champDeSaisie.getText()));
+			System.out.println(AjoutCouleur.ajouterCouleur(monControleur.getTexteAfficher(), champDeSaisie.getText()));
+			//ccc	
 			}
 		});
 		this.add(panSud,BorderLayout.SOUTH);
 		
 		this.setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		FenetrePrincipal  fen=new FenetrePrincipal();
-	}
+	
 
 }
