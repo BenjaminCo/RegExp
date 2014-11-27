5c97ac45c0e226a426c97991dfe76c266c383c5f
Lawlyeth <jpp090@gmail.com>
2014-11-27 17:13:15 +0100

    cc

--- src/Controleur/AjoutCouleur.java
+++ src/Controleur/AjoutCouleur.java
@@ -14,19 +14,40 @@ public class AjoutCouleur {
 	 * @param texteATrouver l'expression régulière qui définit la colorisation.
 	 * @return le texte coloriséau format html comportant les balises correspondantes
 	 */
-	public static String ajouterCouleur(String texteATester, String texteATrouver){
+	public static String ajouterCouleur(String texteATester, String texteATrouver,String couleur){
 		
-		//PEDALE
+		//System.out.println(texteATester);
+		//System.out.println(texteATrouver);
+		
 		 Pattern p = Pattern.compile(texteATrouver);
 		 Matcher m = p.matcher(texteATester);
 		 StringBuffer sb = new StringBuffer();
+		 
+		 
+		 if(couleur.equals("green")){
 		 while (m.find()) {
-		     m.appendReplacement(sb, "<font color='red'>"+m.group()+"</font>");
+			// System.out.println("ololol   " +m.group());
+		     m.appendReplacement(sb, "<font color='green'>"+m.group()+"</font>");
 		 }
+		 }
+		 else {
+			 while (m.find()) {
+				// System.out.println("ololol" +m.group());
+			     m.appendReplacement(sb, "<font color='red'>"+m.group()+"</font>");
+			 }
+		 }
+		 
 		 m.appendTail(sb);
+		//System.out.println("lololol");
+		// sb.replaceAll("<font color='red'><font color='green'>", "</font></font>");
 		return sb.toString();
 		
 	}
+	/*
+	public static void main(String[] args) {
+		System.out.println(ajouterCouleur(null, null, null));
+		
+	}*/
 	
 	
 
