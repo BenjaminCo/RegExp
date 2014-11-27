83993d2e4d7066e21cd092ded3037d2435ea35e0
NathanSantacana <santacana.nathan@gmail.com>
2014-11-27 17:18:10 +0100

    modef-fenetrePrincipale

--- src/Interface/FenetrePrincipal.java
+++ src/Interface/FenetrePrincipal.java
@@ -5,6 +5,8 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.*;
 
@@ -71,6 +73,24 @@ public class FenetrePrincipal extends JFrame {
 			System.out.println(AjoutCouleur.ajouterCouleur(monControleur.getTexteAfficher(), champDeSaisie.getText(),"red"));
 			//ccc	
 			}
+		});
+		
+		champDeSaisie.addKeyListener(new KeyListener() {
+			
+		
+
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				System.err.println("ok");
+				
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {}
+
+			@Override
+			public void keyTyped(KeyEvent e) {}
 		});
 		this.add(panSud,BorderLayout.SOUTH);
 		
