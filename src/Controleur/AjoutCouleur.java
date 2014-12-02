40ea389ffdcc5fa335f4a15e972496d9f225d7d3
Lawlyeth <jpp090@gmail.com>
2014-12-02 21:46:14 +0100

    fonction de fusion (vert + rouge)
    fusion utilisant le string de base , un string vert et un string rouge
    pour faire un string vert et rouge

--- src/Controleur/AjoutCouleur.java
+++ src/Controleur/AjoutCouleur.java
@@ -58,11 +58,146 @@ public class AjoutCouleur {
 		m.appendTail(sb);
 		return "<html>"+sb.toString()+"</html>";
 	}
-	/*
-	public static void main(String[] args) {
-		System.out.println(ajouterCouleur(null, null, null));
+	
+	
+	
+	public static String fusion(String depart, String test1, String test2) 
+		// premier string => sanscouleur
+		// Deuxième string => vert
+		// Troisième string => rouge
+	 {
+		String salut ="";
+		boolean pastrouverg = true;
+		boolean pastrouver1=true;
+		boolean pastrouver2=true;
+		boolean fini = false;
+		//String depart="salut";
+		//String test1="sa<u>l</u>ut"; //é avant après le l
+		//String test2="sal<k>u</k>t"; // t avant après le u
 		
-	}*/
+		// sa<u>l
+		
+		String[] tabdepart = depart.split("");
+		String[] tabdepartfinal = tabdepart;
+		String[] tabtest1 = test1.split("");
+		String[] tabtest2 = test2.split("");
+		
+		int j=0;
+		int intdepart =0;
+		int intdepartfinal=0;
+		int inttabtest1=0;
+		int inttabtest2=0;
+		
+		
+		int intdepartmax =tabdepart.length;
+		int intdepartfinalmax=tabtest2.length;
+		int inttabtest1max=tabtest1.length;
+		int inttabtest2max=tabtest2.length;
+		
+		
+		
+		//System.out.println(tabdepart.toString());
+		//System.out.println(tabdepart.length);
+		
+
+		while (intdepart<tabdepart.length){
+			pastrouverg=true;
+
+			/////////////////////////////////////////////////////////////////////
+			fini = false;
+			
+	
+			
+			//System.out.println("Lettre checkée : " + tabdepart[intdepart]);
+			if(tabtest1[inttabtest1].equals("<")){
+				while(!tabtest1[inttabtest1].equals(">")&&inttabtest1<=inttabtest1max){
+					
+					tabdepartfinal=fonction(intdepartfinal, tabtest1[inttabtest1], tabdepartfinal);
+					intdepartfinal++;
+					inttabtest1++;
+					pastrouverg=false;
+					
+				}
+				tabdepartfinal=fonction(intdepartfinal, tabtest1[inttabtest1], tabdepartfinal);
+				intdepartfinal++;
+				inttabtest1++;
+				pastrouverg=false;
+			}
+
+			
+			/////////////////////////////////////////////////////////////////////
+			fini=false;
+			if(tabtest2[inttabtest2].equals("<")){
+				while(!tabtest2[inttabtest2].equals(">")&&inttabtest2<=inttabtest2max){
+					
+					tabdepartfinal=fonction(intdepartfinal, tabtest2[inttabtest2], tabdepartfinal);
+					intdepartfinal++;
+					inttabtest2++;
+					pastrouverg=false;
+					
+				}
+				tabdepartfinal=fonction(intdepartfinal, tabtest2[inttabtest2], tabdepartfinal);
+				intdepartfinal++;
+				inttabtest2++;
+				pastrouverg=false;
+			}
+
+			
+			
+			/////////////////////////////////////////////////////////////////////
+			
+			
+			
+			
+			if(pastrouverg){
+		
+			intdepart++;
+			inttabtest1++;
+			inttabtest2++;
+			intdepartfinal++;
+			}
+			
+			j=0;
+			
+			//System.out.println("OMAGAD");
+			while(j<tabdepartfinal.length){
+				//System.out.print(tabdepartfinal[j]);
+				
+				j++;
+			}
+			//System.out.println("");
+			//System.out.println("OMAGAD");
+			//System.out.println(" ");
+		}
+		/////FIN WHILE ////////////////////////
+		j=0;
+		while(j<tabdepartfinal.length){
+			salut=salut+tabdepartfinal[j];
+			
+			j++;
+		}
+	
+		//System.out.println("e,voierbv");
+		//System.out.println(salut);
+		//System.out.println("ébvjhsfdbdui");
+		
+		return salut;
+	}
+	
+	public static String [] fonction (int n,String chaine,String []tableau){
+
+	    String[] tab2 = new String [tableau.length+1];
+	    for(int i=0;i<n;i++){
+	        tab2[i]=tableau[i];
+	    }
+	    tab2[n]=chaine;
+	    for(int i=n+1;i< tableau.length+1;i++){
+	        tab2[i]=tableau[i-1];
+	    }
+	    
+		return tab2;
+	    
+	}
 	
 	
 
